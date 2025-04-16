a66586d401251e5c619e778d1baaa06b6a62e0ec
ajitGandhi095 <ajitkumargandhi95@gmail.com>
2025-04-16 23:19:21 +0530

    Initial Commit

---
+++ App1/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, "App1/index.html")
+
+def login(request):
+    msg="Welcome to Travel Point"
+    my_dict= {"msg":msg}
+
+    return render(request, 'App1/login.html', my_dict)
+
+def signup(request):
+    msg="Welcome to Travel Point"
+    my_dict= {"msg":msg}
+
+    return render(request, 'App1/signup.html', my_dict)
+
+def about(request):
+    msg="Welcome to Travel Point"
+    my_dict= {"msg":msg}
+
+    return render(request, 'App1/about.html', my_dict)
+
+def offers(request):
+    msg="Welcome to Travel Point"
+    my_dict= {"msg":msg}
+
+    return render(request, 'App1/offer.html', my_dict)
+
+def news(request):
+    msg="Welcome to Travel Point"
+    my_dict= {"msg":msg}
+
+    return render(request, 'App1/navbar.html', my_dict)
+
+def contact(request):
+    msg="Welcome to Travel Point"
+    my_dict= {"msg":msg}
+    
+    return render(request, 'App1/navbar.html', my_dict)
+
+def homepage(request):
+    return render(request, 'App1/home.html')
